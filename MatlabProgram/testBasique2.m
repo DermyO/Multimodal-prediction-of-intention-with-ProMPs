cfb5f41cb1dd59adaa8e1657dd7ad72d1df69632
DERMY Oriane <oriane.dermy@inria.fr>
2022-05-02 15:21:24 +0200

    adding two simple examples >> unitary tests

--- MatlabProgram/testBasique2.m
+++ MatlabProgram/testBasique2.m
@@ -13,10 +13,10 @@ addpath('./used_functions');
 inputName = {'x'};%label of your inputs
 s_ref=100; %reference number of samples
 nbInput(1) = 1; %number of inputs used during the inference (here Cartesian position)
-M(1) = 10; %number of basis functions to represent nbInput(1)
+M(1) = 50; %number of basis functions to represent nbInput(1)
 
 %This variable is the expected data noise, you can tune this parameter to achieve the trajectory correctly
-expNoise = 0.1;
+expNoise = 0.001;
 percentData = 50; %percent of observed data during the inference
 %type of cost function used to infer the modulation time
 %('MO':model/'ML'maximum likelihood/ 'ME' average/'DI' distance).
@@ -32,8 +32,8 @@ end
 c(1) = 1.0 / (M(1)-1); %center of gaussians
 %avant :h(1) = c(1)/(M(1)); %bandwidth of the gaussians
 s(1) =c(1)/2; %test ori 28/04/2022 car 95 percentile = c(i) + c(i+1) / 2 = mu + 2s => 2s = ci/2 => s = ci/4
-h(1) = 2*s(1)*s(1);
-v =  linspace(5,10,100)'; %fonction à modéliser: y = x/2 + 5 de 5 à 10 en 100 points
+h(1) = 0.005;
+v =  sin(linspace(5,100,100))'; %fonction à modéliser: y = x/2 + 5 de 5 à 10 en 100 points
 t{1}.nbTraj =20;
 t{1}.y = cell(1, t{1}.nbTraj);
 for i=1:t{1}.nbTraj
@@ -41,7 +41,7 @@ for i=1:t{1}.nbTraj
    t{1}.yMat{i} = t{1}.y{i};
    t{1}.totTime(i) = size(t{1}.y{i},1);
    t{1}.alpha(i) = t{1}.totTime(i)/s_ref;
-%   t{1}.realTime{i} = linspace(1,10);
+   t{1}.realTime{i} = linspace(1,10);%TODO AVEC OU SANS CA DOIT MARCHER
 end
 t{1}.nbInput = 1;
 t{1}.label = 'test';
@@ -49,15 +49,18 @@ t{1}.label = 'test';
 [train, test] =  partitionTrajectory(t{1}, 1, percentData, s_ref);
 
 %plot recoverData
-drawRecoverData(t{1}, inputName, 'Specolor','m','namFig', 1);
+%drawRecoverData(t{1}, inputName, 'Specolor','m','namFig', 1);
 
 %compute the distribution for each kind of trajectories.
-promp{1} = computeDistribution(train, M, s_ref,c,h);%, 'Draw');
+promp{1} = computeDistribution(train, M, s_ref,c,h, 'Periodic');
 
 %plot distribution
-%drawDistribution(promp{1}, inputName,s_ref);
+drawDistribution(computeDistribution(train, M, s_ref,c,2*s(1)*s(1)), inputName,s_ref);
+drawDistribution(promp{1}, inputName,s_ref, 'fig',11);
+
 %plot RBF
 %drawBasisFunction(promp{1}.PHI_norm, M);
+%drawBasisFunction(promp{2}.PHI_norm, M);
 
 if (strcmp(choice,'ME')==1)
         expAlpha = promp{1}.mu_alpha;
@@ -72,7 +75,7 @@ end
 display(['expAlpha= ', num2str(expAlpha), ' real alpha= ', num2str(test{1}.alpha)]);
 
 %Recognition of the movement
-infTraj = inference(promp, test{1}, M, s_ref, c, h, test{1}.nbData, expNoise, expAlpha);
+infTraj = inference(promp, test{1}, M, s_ref, c, h, test{1}.nbData, expNoise, expAlpha, 'Periodic');
 
 %draw the infered movement
 drawInference(promp,inputName,infTraj, test{1},s_ref);
